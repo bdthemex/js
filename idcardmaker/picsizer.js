23943317287e67e9e974127264ed173a6f9e1b04
bdthemex <bdthemex@gmail.com>
2026-03-13 00:52:04 +0600

    Update picsizer.js

--- idcardmaker/picsizer.js
+++ idcardmaker/picsizer.js
@@ -1,11 +1,11 @@
 /* Domain Protection Script */
 (function() {
-    // 
-    var allowedDomain = "www.service-bangla.blogspot.com";
+    // আপনার অনুমোদিত ডোমেইন
+    var allowedDomain = "www.bdwapmaster.blogspot.com";
     
-    // 
+    // বর্তমান সাইটের ডোমেইন চেক
     if (window.location.hostname !== allowedDomain) {
-        //ে
+        // যদি না মিলে তবে রিডাইরেক্ট হবে
         window.location.href = "https://" + allowedDomain;
     }
 })();

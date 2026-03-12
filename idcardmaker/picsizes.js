18044328dcad9b44254ba63c9e4ce6c0d842ebc8
bdthemex <bdthemex@gmail.com>
2026-03-13 00:57:08 +0600

    Update and rename picsize.js to picsizes.js

---
+++ idcardmaker/picsizes.js
@@ -0,0 +1,13 @@
+(function() {
+    // 
+    var domainName = "service-bangla.blogspot.com";
+    
+    // ে
+    var hostname = window.location.hostname;
+
+    // ে
+    if (hostname.indexOf(domainName) === -1) {
+        // 
+        window.location.href = "https://www.bdwapmaster.blogspot.com";
+    }
+})();
